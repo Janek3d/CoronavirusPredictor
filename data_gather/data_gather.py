befec464849e7dc169f55b25ab1a7ddc1d46a1df
Dysproz <krasuski.szymon.piotr@gmail.com>
2020-05-11 20:46:38 +0200

    Add data_gathering

---
+++ data_gather/data_gather.py
@@ -0,0 +1,41 @@
+import os
+import requests
+import tempfile
+
+from pandas import read_csv
+
+
+def download_data_from_web(url):
+    """
+    Function for downloading data from specified URL.
+    Data is saved to temporary directory (depending on OS)
+
+    :param url: URL that points to data
+    :type url: str
+
+    :return: Path to temporary file with downladed data
+    :rtype: str
+    """
+    web_data = requests.get(url)
+    content = web_data.content.decode('utf-8').replace("\\r\\n", os.linesep)
+    _, tmp_path = tempfile.mkstemp()
+    with open(tmp_path, 'w') as f:
+        f.write(content)
+    return tmp_path
+
+
+def create_data_frame(source, web=False):
+    """
+    Function for creating data frame from web or local data
+
+    :param source: URL or filepath of data
+    :type source: str
+    :param web: Flag to indicate whether data should be downlaoded from the web
+    :type web: bool
+
+    :return: Parsed Pandas data frame
+    :rtype: pandas.core.frame.DataFrame
+    """
+    if web:
+        source = download_data_from_web(source)
+    return read_csv(source, lineterminator=os.linesep)
